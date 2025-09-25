52c6391fa10dd54bd0cd945be29ebfa656721ccb
JoanMejia <jmejia27@udi.edu.co>
2025-09-25 07:22:48 -0500

    JoanMejia-250925-7:20

--- app/src/main/java/com/example/appmoviljm/MainActivity.java
+++ app/src/main/java/com/example/appmoviljm/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.appmoviljm;
 
 import android.os.Bundle;
-
+//Libreria Importada
+import android.util.Log;
+//
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -12,8 +14,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ///
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        //logcap
+        Log.d("Joan", "onCreate");
+        /// /
+
+
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
